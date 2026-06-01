ae20fa9413bff39ac2c0c7e3b4079c64bfe3e1f8
zangu-online <henryweks1@gmail.com>
2026-06-01 08:12:10 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,8 +2,8 @@ const admin = require("firebase-admin");
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    projectId: process.env.boostyabankkenya,
+    clientEmail: process.env.firebase-adminsdk-fbsvc@boostyabankkenya.iam.gserviceaccount.com,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
   })
 });
